53dc8e99d1f7bb2bc48b6337942c0dd2b9f23fa8
Arno <arnodjenadi@gmail.com>
2024-12-02 17:09:07 +0100

    Sauvegarde fonctionnel

--- scriptsql/save.sh
+++ scriptsql/save.sh
@@ -1,3 +1,4 @@
 # Script pour sauvegarder la BDD #
+host=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb)
 mkdir ~/sae-dolibarr/sauvegarde
-mysqldump -u root -p 'root' dolibarr > ~/sae-dolibarr/sauvegarde/save_$(date +\%Y\%m\%d).sql
+mysqldump -h $host -u root -p'root' dolibarr > ~/sae-dolibarr/sauvegarde/save_$(date +\%Y\%m\%d).sql
